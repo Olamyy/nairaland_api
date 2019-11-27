bc40bbd77ca52e84e1924d50efb6269348ce8d0d
Olamyy <olamyy53@gmail.com>
2019-11-27 13:42:05 +0100

    Removing unused models

--- nairaland/manage.py
+++ nairaland/manage.py
@@ -13,28 +13,5 @@ def cli():
     """Main entry point"""
 
 
-@cli.command("init")
-def init():
-    """Init application, create database tables
-    and create a new user named admin with password admin
-    """
-    from nairaland.extensions import db
-    from nairaland.models import User
-    click.echo("create database")
-    db.create_all()
-    click.echo("done")
-
-    click.echo("create user")
-    user = User(
-        username="admin",
-        email="olamyy53@gmail.com",
-        password="admin",
-        active=True
-    )
-    db.session.add(user)
-    db.session.commit()
-    click.echo("created user admin")
-
-
 if __name__ == "__main__":
     cli()
